e5e4682452bd70fa8ef7095f7938e3bc51cdc41d
arrivu <arrivu@arrivu-Satellite-C675.(none)>
2013-06-21 18:37:53 +0530

    finished filling layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,49 +4,49 @@ describe "StaticPages" do
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home Page" do
    it "should have the content 'Sample App'" do
-visit '/static_pages/home'
+visit root_path
 page.should have_selector('title',
 :text => "#{base_title} | Home")
 end
 it "should have the h1 'Sample App'" do
-visit '/static_pages/home'
+visit root_path
 page.should have_selector('h1', :text => 'Sample App')
 end
   end
 
     describe "Help Page" do
    it "should have the content 'Help'" do
-visit '/static_pages/help'
+visit help_path
 page.should have_selector('title',
-:text => "Ruby on Rails Tutorial Sample App | Help")
+:text => "#{base_title} | Help")
 
 end
 it "should have the h1 'Sample App'" do
-visit '/static_pages/help'
+visit help_path
 page.should have_selector('h1', :text => 'Help')
 end
   end
   describe "About page" do
 it "should have the content 'About Us'" do
-visit '/static_pages/about'
+visit about_path
 page.should have_selector('title',
-:text => "Ruby on Rails Tutorial Sample App | About")
+:text => "#{base_title} | About")
 
 end
 it "should have the h1 'Sample App'" do
-visit '/static_pages/about'
+visit about_path
 page.should have_selector('h1', :text => 'About Us')
 end
 end
  describe "Contact page" do
 it "should have the content 'Contact Us'" do
-visit '/static_pages/contact'
+visit contact_path
 page.should have_selector('title',
-:text => "Ruby on Rails Tutorial Sample App | Contact")
+:text => "#{base_title} | Contact")
 
 end
 it "should have the h1 'Sample App'" do
-visit '/static_pages/contact'
+visit contact_path
 page.should have_selector('h1', :text => 'Contact Us')
 end
 end

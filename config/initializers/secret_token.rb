0eeb500c2e37ad750e297321fa90cc82f7e12fd2
arrivu <arrivu@arrivu-Satellite-C675.(none)>
2013-06-26 14:01:01 +0530

    new changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'bf5a5ed65157ca93681c4b2dfd12399d32aaaacca58d0f3d023e4ad279344ba3e7a91b0c76e45dd948797be67dc6f050becd8df0a5a36f2870c35606d4a74a80'
+SampleApp::Application.config.secret_token = '15a2ea12cf3587bec54b8cb74a807ec440db08c7006a70e2b2801d669d8550d3542d17c48cae44a48aef54edc739346a4e45355970af6ae70e37a67a36e2ab42'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,53 +1,52 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  describe "Home Page" do
-   it "should have the content 'Sample App'" do
-visit root_path
-page.should have_selector('title',
-:text => "#{base_title} | Home")
+describe "Static pages" do
+	it "should have the right links on the layout" do
+	visit root_path
+	click_link "About"
+	page.should have_selector 'title', text: full_title('About Us')
+	click_link "Help"
+	page.should have_selector 'title', text: full_title('Help')
+	click_link "Contact"
+	page.should have_selector 'title', text: full_title('Contact Us')
+	click_link "Home"
+	click_link "Sign up Now!"
+	page.should #
+	click_link "Sample app"
+	page.should #
 end
-it "should have the h1 'Sample App'" do
-visit root_path
-page.should have_selector('h1', :text => 'Sample App')
-end
-  end
 
-    describe "Help Page" do
-   it "should have the content 'Help'" do
-visit help_path
-page.should have_selector('title',
-:text => "#{base_title} | Help")
+subject { page }
 
-end
-it "should have the h1 'Sample App'" do
-visit help_path
-page.should have_selector('h1', :text => 'Help')
-end
-  end
-  describe "About page" do
-it "should have the content 'About Us'" do
-visit about_path
-page.should have_selector('title',
-:text => "#{base_title} | About")
+describe "Home page" do
 
+before { visit root_path }
+it { should have_selector('h1',text:'Sample App') }
+it { should have_selector('title', text: full_title('')) }
+it { should have_selector 'title', text: 'Home' }
 end
-it "should have the h1 'Sample App'" do
-visit about_path
-page.should have_selector('h1', :text => 'About Us')
-end
-end
- describe "Contact page" do
-it "should have the content 'Contact Us'" do
-visit contact_path
-page.should have_selector('title',
-:text => "#{base_title} | Contact")
 
+describe "Help page" do
+
+before { visit help_path }
+it { should have_selector('h1',
+text: 'Help') }
+it { should have_selector('title', text: full_title('Help')) }
 end
-it "should have the h1 'Sample App'" do
-visit contact_path
-page.should have_selector('h1', :text => 'Contact Us')
+
+describe "About page" do
+
+before { visit about_path }
+it { should have_selector('h1',
+text: 'About') }
+it { should have_selector('title', text: full_title('About Us')) }
 end
+
+describe "Contact page" do
+	
+before { visit contact_path }
+it { should have_selector('h1',
+text: 'Contact') }
+it { should have_selector('title', text: full_title('Contact')) }
 end
 end
